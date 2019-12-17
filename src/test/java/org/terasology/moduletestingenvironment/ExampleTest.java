f3a62377fb1abd1131f5db7a5491ab80c3354d66
n.khanipov <n.khanipov@pflb.ru>
2019-12-17 17:45:39 +0300

    Fix guava collection importing

--- src/test/java/org/terasology/moduletestingenvironment/ExampleTest.java
+++ src/test/java/org/terasology/moduletestingenvironment/ExampleTest.java
@@ -15,7 +15,7 @@
  */
 package org.terasology.moduletestingenvironment;
 
-import com.google.api.client.util.Lists;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import org.junit.Assert;
 import org.junit.Test;
