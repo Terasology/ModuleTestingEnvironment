8440363ef874014af889b9d7761b3c2ec30516e3
DarkWeird <foxflameless@gmail.com>
2020-12-01 12:30:17 +0300

    perf(reduce-ram-eats): set ViewDistance to `Legally Blind` for headless clients.

--- src/main/java/org/terasology/moduletestingenvironment/ModuleTestingEnvironment.java
+++ src/main/java/org/terasology/moduletestingenvironment/ModuleTestingEnvironment.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2017 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.moduletestingenvironment;
 
 import com.google.common.base.Preconditions;
@@ -59,6 +46,7 @@ import org.terasology.network.JoinStatus;
 import org.terasology.network.NetworkSystem;
 import org.terasology.registry.CoreRegistry;
 import org.terasology.rendering.opengl.ScreenGrabber;
+import org.terasology.rendering.world.viewDistance.ViewDistance;
 import org.terasology.world.RelevanceRegionComponent;
 import org.terasology.world.WorldProvider;
 
@@ -328,6 +316,8 @@ public class ModuleTestingEnvironment {
      */
     public Context createClient() {
         TerasologyEngine terasologyEngine = createHeadlessEngine();
+        terasologyEngine.getFromEngineContext(Config.class).getRendering().setViewDistance(ViewDistance.LEGALLY_BLIND);
+
         terasologyEngine.changeState(new StateMainMenu());
         connectToHost(terasologyEngine);
         Context context = terasologyEngine.getState().getContext();
