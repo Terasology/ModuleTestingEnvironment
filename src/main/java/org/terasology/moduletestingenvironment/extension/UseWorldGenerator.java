19ae8ad1a18084cb174ed200f77efb3e6e473c31
darkweird <foxflameless@gmail.com>
2021-10-22 13:08:27 +0300

    Revert "fix(checkstyle): fix issues"
    This reverts commit 2b57f0eb3cfd058869376017de82a25311f5f714.

--- src/main/java/org/terasology/moduletestingenvironment/extension/UseWorldGenerator.java
+++ src/main/java/org/terasology/moduletestingenvironment/extension/UseWorldGenerator.java
@@ -17,14 +17,12 @@
 package org.terasology.moduletestingenvironment.extension;
 
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@Inherited
 public @interface UseWorldGenerator {
 
     /**
